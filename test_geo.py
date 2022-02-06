a591db157430c3b27e48337c79653ae62fc49036
Ashutosh Khatri <ashutoshkhatri7@gmail.com>
2022-02-06 19:49:46 +0000

    Edited test_geo 6thFeb2022

--- test_geo.py
+++ test_geo.py
@@ -18,3 +18,31 @@ def test_stationswithinradius():
     stations = build_station_list()
     station_distance = stations_within_radius(stations, (52.2053, 0.1218), 10)
     assert isinstance(station_distance[0],MonitoringStation)
+
+
+
+
+##Task 1D Tests
+from floodsystem.geo import *
+def test_rivers_with_station():
+    stations = build_station_list()
+    function_test = rivers_with_station(stations)
+    assert "Par River" in function_test
+
+
+def test_stations_by_river():
+    stations = build_station_list()
+    function_test = stations_by_river(stations)
+    assert "River Tees" in function_test
+
+
+
+##Task 1E Tests
+sample_data = [ ('River Great Ouse', 30), ('River Derwent', 25), ('River Thames', 55), ('River Avon', 31), ('River Aire', 24), ('River Calder', 23), ('River Severn', 21), ('River Stour', 21), ('River Ouse', 18), ('River Colne', 18)]
+
+def test_rivers_by_station_number():
+    stations = sample_data
+    function_test = rivers_by_station_number(stations,1)
+    assert "River Thames" in function_test
+
+
