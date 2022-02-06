cd252063edf12e3ce81471e0472966ecaeb2ae9a
Amelia Weir <asew4@cam.ac.yk>
2022-02-06 17:27:31 +0000

    commit geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,31 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+from floodsystem.stationdata import build_station_list
+from .station import MonitoringStation
+from . import datafetcher
+
+def stations_by_distance(stations, p):
+    #Function to sort list of stations and their distance from a coordinate sorted by increasing distance
+    stations = build_station_list()
+    names = []
+    distance = []
+    for station in stations:
+        names.append(station.name)
+        distance.append(haversine(p, station.coord))
+    overall_list = list(zip(names, distance))
+    overall_list = sorted_by_key(overall_list,1)
+    return overall_list
+
+def stations_within_radius(stations, centre, r):
+    #Function to name the stations that are within a radius, r, of a coordinate centre.
+    stations = build_station_list()
+    names =[]
+    for station in stations:
+        if haversine(centre, station.coord) <= r:
+            names.append(station.name)
+        else:
+            pass
+    return names
+
