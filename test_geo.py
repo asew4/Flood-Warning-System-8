7684106647d59194b20cf84f8fad92939a21993d
Amelia Weir <asew4@cam.ac.yk>
2022-02-06 19:19:59 +0000

    update to test

--- test_geo.py
+++ test_geo.py
@@ -11,3 +11,10 @@ def test_stationsbydistance():
     assert isinstance(station_distance[0][1],float)
 
 #Test for stations_within_radius function (Task 1C)
+from floodsystem.geo import stations_within_radius
+from floodsystem.station import MonitoringStation
+
+def test_stationswithinradius():
+    stations = build_station_list()
+    station_distance = stations_within_radius(stations, (52.2053, 0.1218), 10)
+    assert isinstance(station_distance[0],MonitoringStation)
