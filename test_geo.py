4d628f5fc6319ea0cffb23a7a666c52876a89e49
Ashutosh Khatri <ashutoshkhatri7@gmail.com>
2022-02-07 14:58:43 +0000

    Merge branch 'main' of https://github.com/asew4/Flood-Warning-System-8

--- test_geo.py
+++ test_geo.py
@@ -37,7 +37,7 @@ station3 = MonitoringStation(station_id='stat3',
                              label='Station 3',
                              coord=(4., 5.),
                              typical_range=(0., 1.),
-                             river='riv3',
+                             river='riv1',
                              town='town3')
 station4 = MonitoringStation(station_id='stat4',
                              river='riv1',
@@ -59,42 +59,28 @@ station6 = MonitoringStation(station_id='stat6',
                              coord=(4., 5.),
                              typical_range=(0., 1.),
                              river='riv6',
-                             town='town6')                               
+                             town='town6')       
+station7 = MonitoringStation(station_id='stat6',
+                             measure_id='test_measure_id_1',
+                             label='Station 6',
+                             coord=(4., 5.),
+                             typical_range=None,
+                             river='riv6',
+                             town='town6')                             
 
 ##Task 1D Tests
 from floodsystem.geo import *
 
 def test_river_by_station_number():
-    station1 = MonitoringStation(station_id='stat1',
-                                 river='riv1',
-                                 measure_id='test_measure_id_1',
-                                 label=' Station 1',
-                                 coord=(4., 5.),
-                                 typical_range=(0., 1.),
-                                 town='town1')
-    station2 = MonitoringStation(station_id='stat2',
-                                 measure_id='test_measure_id_1',
-                                 label=' Station 1',
-                                 coord=(4., 5.),
-                                 typical_range=(0., 1.),
-                                 river='riv1',
-                                 town='town2')
-    station3 = MonitoringStation(station_id='stat3',
-                                 measure_id='test_measure_id_1',
-                                 label=' Station 1',
-                                 coord=(4., 5.),
-                                 typical_range=(0., 1.),
-                                 river='riv3',
-                                 town='town3')
     stations = [station1, station2, station3]
     function_test = rivers_by_station_number(stations,1)
-    assert ('riv1',2) in function_test
+    assert ('riv1', 2) in function_test
 
 def test_rivers_with_stations():
     stations = [station1, station2, station3]
     function_test = rivers_with_stations(stations)
     assert "riv1" in function_test
-    assert len(function_test) == 3
+    assert len(function_test) == 2
 
 
 def test_stations_by_river():
@@ -103,6 +89,9 @@ def test_stations_by_river():
     function_test = stations_by_river(stations)
     assert 'Station 1' in ((stations_by_river(stations))['riv1'])
     assert 'Station 4' in ((stations_by_river(stations))['riv1'])
+
+
+
 
 
 
