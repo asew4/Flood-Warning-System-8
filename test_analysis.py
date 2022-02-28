0458ed7bc53845e69a18ac659b4a10df6b9f1ea3
Amelia Weir <asew4@cam.ac.yk>
2022-02-28 00:38:38 +0000

    Update

--- test_analysis.py
+++ test_analysis.py
@@ -1,10 +1,11 @@
+from os import times
 from floodsystem import stationdata
 import pytest
 import numpy as np
 from floodsystem import datafetcher
 from datetime import timedelta
 from floodsystem import analysis
-import matplotlib
+from matplotlib import dates
 
 stations = stationdata.build_station_list()
 
@@ -12,9 +13,9 @@ stationdata.update_water_levels(stations)
 
 def test_analysis():
     dt = 10
-    dates, levels = datafetcher.fetch_measure_levels(stations[0].measure_id, dt=timedelta(days=dt))
-    x= matplotlib.dates.date2num(dates)
+    times, levels = datafetcher.fetch_measure_levels(stations[0].measure_id, dt=timedelta(days=dt))
+    x= dates.date2num(times)
 
-    poly, date_0 = analysis.polyfit(dates, levels, 2)
+    poly, date_0 = analysis.polyfit(times, levels, 2)
     assert date_0 == x[0]
 
