0d7d54a10581264ab494b35db0b0de3c51727770
Amelia Weir <asew4@cam.ac.yk>
2022-02-28 00:06:05 +0000

    Updates to E and F

--- Task2E.py
+++ Task2E.py
@@ -10,24 +10,24 @@ from datetime import datetime, timedelta
 def highest_stations(n, stations):
     stationdata.update_water_levels(stations)
     highest_levels = []
-    for station in stations:
+    for station in stations: #putting stations into list of tuples of (station, latest level)
         if station.latest_level == None:
             pass
-        elif station.latest_level >= 90:
+        elif station.name == "Letcombe Bassett":
             pass
         else:
-            highest_levels.append((station, station.latest_level))
-        sorted_highest_levels = utils.sorted_by_key(highest_levels, 1)
-    return sorted_highest_levels[-n:]
+            highest_levels.append((station, station.latest_level)) 
+        sorted_highest_levels = utils.sorted_by_key(highest_levels, 1) #Sorting list into order of increasing level
+    return sorted_highest_levels[-n:] #Returns list of n stations with highest levels
     
 
 
 def run():
     stations = stationdata.build_station_list()
-    five_stations = highest_stations(5, stations)
+    five_stations = highest_stations(5, stations) #List of 5 stations with highest level and their repective level (tuple)
     dt = 10
     for i in five_stations:
-        dates, levels = datafetcher.fetch_measure_levels(i[0].measure_id, dt=timedelta(days=dt))
+        dates, levels = datafetcher.fetch_measure_levels(i[0].measure_id, dt=timedelta(days=dt)) #Retrieving data for last dt days for each of stations in list
         plot.plot_water_levels(i[0], dates, levels)
 
 
--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -4,11 +4,11 @@ import matplotlib
 
 def polyfit(dates, levels, p):
     
-    x = matplotlib.dates.date2num(dates)
+    x = matplotlib.dates.date2num(dates)    #Returns list of floats rather than list of datetime
     y = levels
 
-    p_coeff = np.polyfit(x - x[0], y, p)
+    p_coeff = np.polyfit(x - x[0], y, p) #Using shifted values of x, finds coefficients of best-fit polynomial f(x) of degree p
 
-    poly = np.poly1d(p_coeff)
+    poly = np.poly1d(p_coeff) #Converts coefficient into polynomial that can be evaluated
 
     return poly, x[0]
