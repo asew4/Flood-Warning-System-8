fdc75f48aebeaba93dee4900a9f0d43abcb56f68
Amelia Weir <asew4@cam.ac.yk>
2022-02-28 00:30:34 +0000

    Update

--- test_analysis.py
+++ test_analysis.py
@@ -12,9 +12,9 @@ stationdata.update_water_levels(stations)
 
 def test_analysis():
     dt = 10
-    dates, levels = datafetcher.fetch_measure_levels(stations[0].measure_id, dt=timedelta(days=dt))
-    #x = matplotlib.dates.date2num(dates)
+    times, levels = datafetcher.fetch_measure_levels(stations[0].measure_id, dt=timedelta(days=dt))
+    #x = matplotlib.dates.date2num(times)
 
-    poly, date_0 = analysis.polyfit(dates, levels, 2 )
+    poly, date_0 = analysis.polyfit(times, levels, 2 )
     #assert date_0 == x[0]
     #assert isinstance(poly, np.poly1d)
